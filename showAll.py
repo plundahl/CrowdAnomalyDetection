313722eb71999983bcbf592339fefd0fcedf77f8
Petter Lundahl <plundahl@kth.se>
2017-10-27 12:43:42 +0200

    Move read to function

--- showAll.py
+++ showAll.py
@@ -3,22 +3,41 @@ from math import cos, sin, radians
 
 length = 10
 
-plt.arrow(0.2, 0, 0.5, 0.5)
-plt.arrow(2, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
+#plt.arrow(0.2, 0, 0.5, 0.5)
+#plt.arrow(2, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
 
 lista = []
+"""
 with open("students001.vsp") as f:
     for line in f:
         word = line.split()
         if(len(word) == 8):
             lista.append([float(word[0]),float(word[1]),radians(float(word[3]))])
-"""
 with open("students003.vsp") as f:
     for line in f:
         word = line.split()
         if(len(word) == 8):
             lista.append([float(word[0]),float(word[1]),radians(float(word[3]))])
 """
+
+def reader(file, list):
+    print("hej")
+    file.readline()
+    line = file.readline()
+    while line:
+        points = int(line.split()[0])
+        line=file.readline()
+        for x in range(1, points):
+            line=file.readline()
+            word = line.split()
+            lista.append([float(word[0]),float(word[1]),radians(float(word[3]))])
+        line = file.readline()
+
+
+with open("students001.vsp") as f:
+    reader(f, lista)
+
+
 prev = []
 for point in lista:
     plt.arrow(point[0],point[1],length*sin(point[2]),length*cos(point[2]), head_width=5)
