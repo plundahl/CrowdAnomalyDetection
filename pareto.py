52dee3c63e79ec3f22663cfc84e1f49e7666724f
Petter Lundahl <plundahl@kth.se>
2017-11-08 11:16:33 +0100

    Failed test with own tree consept

--- pareto.py
+++ pareto.py
@@ -1,23 +1,38 @@
 import matplotlib.pyplot as plt
-import random
 from math import cos, sin, radians, floor
+import time
+import random
+import cProfile
 
-length = 1000
+length = 4000
 xs = []
 ys = []
+xs2 = []
+ys2 = []
 S = []
 n = []
 F = [[]]
 
+#plt.subplot(121)
+random.seed(2)
+
 for x in range (0,length):
-    xs.append(random.uniform(1,10000))
-    ys.append(random.uniform(1,10000))
+    xs.append(random.uniform(1,100))
+    ys.append(random.uniform(1,100))
+    xs2.append(random.uniform(1,100))
+    ys2.append(random.uniform(1,100))
     S.append([])
     n.append(0)
+#    print([x, xs[x], ys[x]])
 plt.plot(xs,ys,'x')
 
+dom_tests = 0
 def dominates(a,b):
-    return xs[a] <= xs[b] and ys[a] <= ys[b] and (xs[a] < xs[b] or ys[a] < ys[b])
+    global dom_tests
+    dom_tests = dom_tests + 1
+    #return xs[a] <= xs[b] and ys[a] <= ys[b] and (xs[a] < xs[b] or ys[a] < ys[b])
+    return xs[a] <= xs[b] and ys[a] <= ys[b] and xs2[a] <= xs2[b] and ys2[a] <= ys2[b] and (xs[a] < xs[b] or ys[a] < ys[b] or xs2[a] < xs2[b] or ys2[a] < ys2[b])
+
 
 for p in range(0,length):
     for q in range(0, length):
@@ -36,10 +51,147 @@ while F[i]:
             n[q] = n[q] - 1
             if n[q] == 0:
                 H.append(q)
-        if i <= 5:
-            plt.plot(xs[p],ys[p],'o', color=str(i*0.2))
+        if i <= 9:
+            plt.plot(xs[p],ys[p],'o', color='C'+str(i%10))
     i=i+1
     F.append(H)
 
+print("--- FAST: %s ---" % dom_tests)
+dom_tests = 0
+
+class Leaf(object):
+    def __init__(self, index):
+        self.index = index
+        self.children = []
+    
+    def add(self, newLeaf, log=False):
+        if log:
+            print(str(self.index) + " ADD " + str(newLeaf.index))
+            for child in self.children:
+                print("#" + str(child.index))
+                
+        dominated = []
+        random.shuffle(self.children)
+        for child in self.children:
+            if log:
+                print(">" + str(child.index))
+            if dominates(child.index, newLeaf.index):
+                if log:
+                    print("dominated")
+                child.add(newLeaf)
+                return
+            elif dominates(newLeaf.index, child.index):
+                if log:
+                    print("dominates")
+                dominated.append(child)
+                newLeaf.add(child)
+        for child in dominated:
+            self.children.remove(child)
+        self.children.append(newLeaf)
+        return
+
+    def merge(self):
+        if len(self.children) == 1:
+            self.children = self.children[0].children
+        else:
+            if len(self.children) > 1:
+                self.children = self.mergeLists([self.children[i:i + 2] for i in range(0, len(self.children), 2)])
+                self.merge()
+
+    def mergeLists(self, pairs=[]):
+        tmpList=[]
+        for pair in pairs:
+            if len(pair) == 1:
+                tmpList.append(pair[0])
+            else:
+                merged = []
+                for child1 in pair[0].children:
+                    add = True
+                    for child2 in pair[1].children:
+                        if dominates(child2.index, child1.index):
+                            child2.add(child1)
+                            add = False
+                            break
+                    if add:
+                        merged.append(child1)
+                for child1 in pair[1].children:
+                    add = True
+                    for child2 in pair[0].children:
+                        if dominates(child2.index, child1.index):
+                            child2.add(child1)
+                            add = False
+                            break
+                    if add:
+                        merged.append(child1)
+                tmpLeaf = Leaf(-2)
+                tmpLeaf.children = merged
+                tmpList.append(tmpLeaf)
+        return tmpList
+
+    def print(self):
+        tmp = []
+        for child in self.children:
+            tmp.append(child.print())
+        return {str(self.index) : tmp}
+
+    def depth(self):
+        if len(self.children) == 0:
+            return 1
+        tmp = []
+        for child in self.children:
+            tmp.append(child.depth())
+        return max(tmp) + 1
+
+    def size(self):
+        tmp = []
+        for child in self.children:
+            tmp.append(child.depth())
+        return sum(tmp) + 1
+
+start_time = time.time()
+
+def treePareto():
+    Root = Leaf(-1)
+    for p in range(0,length):
+        tmp = Leaf(p)
+        Root.add(tmp)
+    print("--- First TREE: %s ---" % dom_tests)
+
+    #for child in Root.children:
+    #    print("--- %s depth ---" % child.depth())
+    #    print("--- %s size ---" % child.size())
+
+    #plt.subplot(122)
+    i = 0
+    while(len(Root.children) > 0):
+        tmp = []
+        tmpRoot = Leaf(-1)
+        for child in Root.children:
+            tmp.append(child.index)
+        #    for child2 in child.children:
+        #        tmpRoot.add(child2)
+            #if i <= 9:
+                #plt.plot(xs[child.index]+1,ys[child.index]+1,'o', color='C'+str(i%10))
+        #Root = tmpRoot
+        Root.merge()
+        #print({i:sorted(tmp)})
+        if sorted(tmp) != sorted(F[i]):
+            print("ERROR!! on front %s" % i)
+            exit()
+        i = i + 1
+    if(i+1 != len(F)):
+        print("ERROR!! F:{} != TREE:{}".format(len(F), i+1))
+        exit()
+    print("--- TREE: %s ---" % dom_tests)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+#cProfile.run("treePareto()")
+
+treePareto()
+i = 0
+for x in F:
+    #print({i:sorted(x)})
+    i = i + 1
+
 #plt.axis([0,2*xmax,0,2*ymax])
-plt.show()
+#plt.show()
