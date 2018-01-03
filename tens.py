39905907fd6a0f4d653a2b3ced0e5a1177ee1998
Petter Lundahl <plundahl@kth.se>
2018-01-03 11:53:58 +0100

    Add T-ENS version of non-dominated sorting

---
+++ tens.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+from math import cos, sin, radians, floor
+import time
+import random
+import cProfile
+
+class Node(object):
+    def __init__(self, index: int, dyads: list, size):
+        self.index = index
+        self.size = size
+        self.dyad = dyads
+        self.objSeq = list(range(1,size))
+        random.shuffle(self.objSeq)
+        self.branch = [None]*size
+
+    def getList(self):
+        result = [self.index]
+        for elem in self.branch:
+            if(elem != None):
+                result = result + elem.getList()
+        return result
+
+def tens(population: list):
+    population = sorted(population, key=lambda x: x.dyad[0])
+    F = []
+    k = 0
+    #for i in range(1):
+    while(len(population) > 0):
+        deleted = []
+        F.append(population[0])
+        for p in population:
+            if(update_tree(p,F[k])):
+                deleted.append(p)
+
+        tmp=[]
+        for p in deleted:
+            tmp.append(p.index)
+            population.remove(p)
+        k = k+1
+    return F
+
+def update_tree(p: Node, tree: Node):
+    if(p == tree):
+        return True
+    elif check_tree(p, tree, True):
+        return True
+    return False
+
+def check_tree(p: Node, tree: Node, add_pos: bool):
+    if(tree==None):
+        return True
+    
+    m = find_min_m(p, tree)
+    if(m == -1):
+        return False
+    else:
+        for i in range(m+1):
+            if(check_tree(p, tree.branch[i], i==m and add_pos) == False):
+                return False
+        if(tree.branch[m] == None and add_pos):
+            tree.branch[m] = p
+        return True
+
+def find_min_m(p: Node, tree: Node):
+    for m in range(p.size-1):
+        if(p.dyad[tree.objSeq[m]] < tree.dyad[tree.objSeq[m]]):
+            return m
+    return -1
+
